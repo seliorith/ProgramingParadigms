7a274f3103917601a85775633d987390ec2e1711
Dmitrii Elkin <yolkindm@mail.ru>
2024-03-04 16:01:38 +0300

    First commit

---
+++ HW1/HW1_1.py
@@ -0,0 +1,14 @@
+# Задача №1
+# Дан список целых чисел numbers. Необходимо написать в императивном стиле процедуру для
+# сортировки числа в списке в порядке убывания. Можно использовать любой алгоритм сортировки
+def bubble_sort_descending(numbers):
+    n = len(numbers)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if numbers[j] < numbers[j + 1]:
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+
+
+numbers = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort_descending(numbers)
+print('Сортировка по убыванию:', numbers)
---
+++ HW1/HW1_2.py
@@ -0,0 +1,10 @@
+﻿# Задача №2
+# Написать точно такую же процедуру, но в декларативном стиле
+def sort_descending(numbers):
+    return sorted(numbers, reverse=True)
+
+
+# Пример использования:
+numbers = [64, 34, 25, 12, 22, 11, 90]
+sorted_descending_numbers = sort_descending(numbers)
+print("Сортировка по убыванию:", sorted_descending_numbers)
